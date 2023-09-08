bc8c77c003cc99e7ee25b5797e70c848b30f800d
aymeric <rambaudaymeric@gmail.com>
2023-09-08 14:05:53 +0200

    aaa

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 console.log("hello");
 
+import Sequelize from 'sequelize';
+
+
+
 import express from 'express'
 import "dotenv/config"
 
@@ -9,6 +13,12 @@ const PORT = process.env.PORT as string;
 app.get('/helloo', (_, res) => {
     console.log("hello les toutous");
     res.send("ok")
+})
+
+app.get('/nombre_alea', (_, res) => {
+  let a = Math.floor(Math.random()*101)
+  console.log(a);
+  res.send("tirage " + a);
 })
 
 app.listen( parseInt(PORT), () =>
